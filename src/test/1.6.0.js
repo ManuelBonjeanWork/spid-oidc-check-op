54225b5dafa6416474a0f28637c657be7031325f
lorenzo.r.sanua.external <lorenzo.r.sanua@accenture.com>
2023-03-29 16:31:54 +0200

    first commit

--- src/test/1.6.0.js
+++ src/test/1.6.0.js
@@ -1,22 +1,31 @@
 const TestMetadata = require('../server/lib/test/TestMetadata.js');
 
 class Test_1_6_0 extends TestMetadata {
+  constructor(metadata) {
+    super(metadata);
+    this.num = '1.6.0';
+    this.description = 'The jwks SHOULD contain x5c';
+    this.validation = 'automatic';
+  }
 
-    constructor(metadata) {
-        super(metadata);
-        this.num = "1.6.0";
-        this.description = "The jwks SHOULD contain x5c";
-        this.validation = "self";
-    }
+  async exec() {
+    super.exec();
 
-    async exec() {
-        super.exec();
-        
-        this.notes = "TODO automatic";
+    let jwks;
+    if (!this.metadata.configuration.jwks.keys.some((key) => key.x5c)) {
+      jwks = (await axios.get(this.metadata.configuration.jwks_uri)).data;
+      if (!jwks.keys.includes('x5c')) {
+        this.notes = "x5c is not present, it's recommended but not mandatory";
         return true;
-
+      }
+    } else {
+      this.notes = this.metadata.configuration.jwks.keys;
+      return true;
     }
 
+    this.notes = 'x5c=' + jwks['x5c'];
+    return true;
+  }
 }
 
-module.exports = Test_1_6_0
+module.exports = Test_1_6_0;
